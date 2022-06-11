9709698fae70ea63b0c21cc08a25bd131d13bda9
sofip <sofipaniale@gmail.com>
2022-06-11 10:56:16 -0300

    potencia y modulo

---
+++ archivos/modulo.py
@@ -0,0 +1,6 @@
+def modulo(a,b):
+  if b != 0:
+    modulo= a%b
+    print (modulo)
+  else: 
+    print ('El segundo argumento debe ser distinto de 0')
---
+++ archivos/potencia.py
@@ -0,0 +1,5 @@
+def potencia(a,b):
+    potencia= a**b
+    print (potencia)
+    
+potencia(0,3)
