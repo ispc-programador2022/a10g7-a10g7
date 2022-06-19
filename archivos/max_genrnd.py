92d90c2a4cfc6e20f33e7006d95562139f18a1ac
Franot <105898345+Franot@users.noreply.github.com>
2022-06-19 18:08:16 -0300

    Update max_genrnd.py

--- archivos/max_genrnd.py
+++ archivos/max_genrnd.py
@@ -1,4 +1,5 @@
-from genrnd import genrnd
-lista_num=genrnd()
-num_max=max(lista_num)
-print('El valor maximo es:', num_max)}
+def max_genrnd():
+    from genrnd import genrnd
+    lista_num=genrnd()
+    num_max=max(lista_num)
+    print('El valor maximo es:', num_max)
