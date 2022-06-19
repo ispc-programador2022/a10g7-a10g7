6707b2e603690a2dd5655332bedb072cce822654
Franot <105898345+Franot@users.noreply.github.com>
2022-06-19 18:08:52 -0300

    Update min_genrnd.py

--- archivos/min_genrnd.py
+++ archivos/min_genrnd.py
@@ -1,4 +1,5 @@
-from genrnd import genrnd
-lista_num=genrnd()
-num_min=min(lista_num)
-print('El valor minimo es:', num_min)
+def min_genrnd():
+    from genrnd import genrnd
+    lista_num=genrnd()
+    num_min=min(lista_num)
+    print('El valor minimo es:', num_min)
